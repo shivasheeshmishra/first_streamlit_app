27e271f820ae6c735e925170812a84f23243e14d
shivasheeshmishra <140915282+shivasheeshmishra@users.noreply.github.com>
2023-07-30 00:43:01 +0530

    added some texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner');
+streamlit.header('Breakfast Menu');
+streamlit.text('Omega 3 and buleberry oatsmeal');
+streamlit.text('Kale, spinach and Rocket smoothie');
+streamlit.text('Hard-boiled and free-range egg');
