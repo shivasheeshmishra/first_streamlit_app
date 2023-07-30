33601f1a1cea79442339cd2ebfa7931163aa7e86
shivasheeshmishra <140915282+shivasheeshmishra@users.noreply.github.com>
2023-07-31 04:41:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ try:
 #import requests
     else:
       back_from_function = get_fruityvice_data(fruit_choice);
-        streamlit.dataframe(back_from_function);
+      streamlit.dataframe(back_from_function);
 except URLError as e:
   streamlit.error();
 
