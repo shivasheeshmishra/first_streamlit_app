6bce214e62dcb7e7e14d3400882661a7f7dd2e75
shivasheeshmishra <140915282+shivasheeshmishra@users.noreply.github.com>
2023-07-31 03:58:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,4 +39,7 @@ my_data_rows = my_cur.fetchall();
 streamlit.header("FRUIT LOAD LIST CONTAINS :");
 streamlit.dataframe(my_data_rows);
 
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Apple');
+
+
 
