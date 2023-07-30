85911f9ff559c221f02a27aed2da9874bb7823a1
shivasheeshmishra <140915282+shivasheeshmishra@users.noreply.github.com>
2023-07-31 05:25:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,12 +13,9 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 #import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
 my_fruit_list1 = my_fruit_list.set_index('Fruit');
-
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list1.index),['Avocado','Strawberries']);
-
 fruits_to_show = my_fruit_list1.loc[fruits_selected];
-
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show);
 # Create a function
@@ -26,27 +23,19 @@ def get_fruityvice_data (this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice);
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json());
     return fruityvice_normalized;
-
 streamlit.header("Fruityvice Fruit Advice!");
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?');
     if not fruit_choice:
       streamlit.error("Please select a fruit to get information");
-
 #streamlit.write('The user entered ', fruit_choice);
-
-
-
 #import requests
     else:
       back_from_function = get_fruityvice_data(fruit_choice);
       streamlit.dataframe(back_from_function);
 except URLError as e:
   streamlit.error();
-
 #import snowflake.connector
-
-
 streamlit.header("View Our FRUIT LIST And Add Your Favorites :");
 #Snowflake-related-function
 def get_fruit_load_list():
