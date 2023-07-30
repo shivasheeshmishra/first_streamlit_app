415f592c0da4b7dcddb4a3986159075be1aed660
shivasheeshmishra <140915282+shivasheeshmishra@users.noreply.github.com>
2023-07-31 03:48:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ streamlit.dataframe(fruityvice_normalized);
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]);
 my_cur = my_cnx.cursor();
-my_cur.execute("SELECT * from pc_rivery_db.public.fruit_load_list");
+my_cur.execute("SELECT * from PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST");
 my_data_row = my_cur.fetchone();
 streamlit.text("FRUIT LOAD LIST CONTAINS :");
 streamlit.text(my_data_row);
