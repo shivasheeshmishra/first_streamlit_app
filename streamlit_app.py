266a46c404fadf704e40a9c13faefbf11393c098
shivasheeshmishra <140915282+shivasheeshmishra@users.noreply.github.com>
2023-07-30 22:55:49 +0530

    Added one more line

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner');
 streamlit.header('Breakfast Menu');
-streamlit.text('Omega 3 and buleberry oatsmeal');
-streamlit.text('Kale, spinach and Rocket smoothie');
-streamlit.text('Hard-boiled and free-range egg');
+streamlit.text('🥣 Omega 3 and buleberry oatsmeal');
+streamlit.text('🥗 Kale, spinach and Rocket smoothie');
+streamlit.text('🐔 Hard-boiled and free-range egg');
+streamlit.text('🥑🍞 Avocodo toast');
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
